d171ff91f426c1ce4cca77f41ab713eea3b55c60
halee99 <577916577@qq.com>
2019-08-23 17:36:50 +0800

    fix

--- ch04/ch4_mceval.rkt
+++ ch04/ch4_mceval.rkt
@@ -59,14 +59,14 @@
 (define (eval-assignment exp env)
   (set-variable-value! (assignment-variable exp)
                        (eval (assignment-value exp) env)
-                       env))
-  ; 'ok)
+                       env)
+  'ok)
 
 (define (eval-definition exp env)
   (define-variable! (definition-variable exp)
                     (eval (definition-value exp) env)
-                    env))
-  ; 'ok)
+                    env)
+  'ok)
 
 ;;;SECTION 4.1.2
 
