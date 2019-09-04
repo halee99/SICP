4d7083e472562cfd267441c0c353f28cf13c64db
halee99 <577916577@qq.com>
2019-09-04 13:58:44 +0800

    fix eval-sequence

--- ch04/ch4-leval.rkt
+++ ch04/ch4-leval.rkt
@@ -72,7 +72,7 @@
           "Unknown procedure type -- APPLY" procedure))))
 
 (define (eval-sequence exps env)
-  (cond ((last-exp? exps) (actual-value (first-exp exps) env))
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
         (else (actual-value (first-exp exps) env)
               (eval-sequence (rest-exps exps) env))))
 
